ec8c8143dda2e1ab569e1a373ffa370dc9e53cc2
Matthijs de Goede <m.b.j.degoede@student.tudelft.nl>
2023-11-26 23:46:47 +0100

    Configure semitruck spawn rates for most of the of european countries

---
+++ retrieve_cabotage_stats.py
@@ -0,0 +1,67 @@
+import openpyxl
+
+# TODO: add link to sheet
+file_path = 'road_go_ca_hac_spreadsheet.xlsx'
+workbook = openpyxl.load_workbook(file_path)
+country_data = {}
+
+
+def process_sheet(sheet):
+    country_of_interest = sheet['C7'].value.lower()
+
+    data = []
+    for row in sheet.iter_rows(min_row=12, max_row=41, min_col=1, max_col=49):
+        country = row[0].value
+        tonnage = row[47].value
+
+        if tonnage != ":":
+            data.append((country, float(tonnage)))
+
+    if data:
+        # sort the data by tonnage
+        sorted_data = sorted(data, key=lambda x: x[1], reverse=True)
+
+        # get the top 10 performing countries
+        top = sorted_data[:10]
+
+        # calculate total tonnage for all countries
+        total_tonnage = sum(entry[1] for entry in sorted_data)
+
+        # make a dictionary with the top 10, and the corresponding percentages
+        country_data[country_of_interest] = {}
+        for entry in top:
+            percentage = round((entry[1] / total_tonnage), ndigits=2)
+            country_data[country_of_interest][entry[0].lower()] = percentage
+
+
+# process each sheet
+for sheet_name in workbook.sheetnames:
+    if sheet_name.startswith("Sheet") and float(sheet_name.split(" ")[1]) <= 40:
+        sheet = workbook[sheet_name]
+        process_sheet(sheet)
+
+for key in country_data:
+    print(f"\n{key}\n{country_data[key]}\n")
+
+
+def retrieve_scaled(scale_factor, country):
+    sum = 0
+    scaled_dict = {}
+    for foreign_country in country_data[country]:
+        sum += country_data[country][foreign_country]
+    for foreign_country in country_data[country]:
+        scaled_dict[foreign_country] = round(country_data[country][foreign_country] / sum * scale_factor, 2)
+    return scaled_dict
+
+
+def print_scaled_dict(scaled_dict):
+    res_string = "["
+    for index, country in enumerate(scaled_dict.keys()):
+        res_string += f"(\"{country}\", {scaled_dict[country]})"
+        if index < len(scaled_dict.keys()) - 1:
+            res_string += ", "
+    res_string += "]"
+    print(res_string)
+
+
+print_scaled_dict(retrieve_scaled(0.3, "slovakia"))
--- semi_vanilla.py
+++ semi_vanilla.py
@@ -39,171 +39,186 @@ vanilla_truck_list = [
 ]
 
 vanilla_trailer_list = [
-    "scs_curtain_traffic", #2 variants
-    "scs_reefer_traffic", #1 variant
-    "scs_dryvan_traffic", #1 variant
-    "scs_dryvan_mfloor_traffic", #1 variant
-    "scs_foodtank_traffic", #2 variants
-    "scs_chemtank_traffic", #1 variant
-    "scs_silo_traffic", #1 variant
-    "scs_aero_dynamic_traffic", #1 variant
-    "car_transporter_traffic", #4 variants
-    "scs_gooseneck_traffic", #5 variants
-    "scs_flatbed_brick_traffic", #9 variants
+    "scs_curtain_traffic",  # 2 variants
+    "scs_reefer_traffic",  # 1 variant
+    "scs_dryvan_traffic",  # 1 variant
+    "scs_dryvan_mfloor_traffic",  # 1 variant
+    "scs_foodtank_traffic",  # 2 variants
+    "scs_chemtank_traffic",  # 1 variant
+    "scs_silo_traffic",  # 1 variant
+    "scs_aero_dynamic_traffic",  # 1 variant
+    "car_transporter_traffic",  # 4 variants
+    "scs_gooseneck_traffic",  # 5 variants
+    "scs_flatbed_brick_traffic",  # 9 variants
 ]
 
 spawn_config = {
     "netherlands": {
         "national": 0.60,
-        "international": [("poland", 0.10), ("romania", 0.06), ("lithuania", 0.04), ("germany", 0.04), ("belgium", 0.03), ("france", 0.02), ("uk", 0.01)],
+        "international": [("poland", 0.10), ("romania", 0.04), ("germany", 0.05), ("belgium", 0.04),
+                          ("lithuania", 0.02), ("france", 0.01), ("uk", 0.01), ("spain", 0.01), ("hungary", 0.01),
+                          ("luxembourg", 0.01)],
         "random": 0.10,
     },
     "germany": {
-        "national": 0.79,
-        "international": [("netherlands", 0.04), ("switzerland", 0.02), ("austria", 0.02), ("czech", 0.04),
-                          ("poland", 0.04), ("france", 0.02), ("denmark", 0.02)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("poland", 0.14), ("netherlands", 0.03), ("lithuania", 0.03), ("romania", 0.02),
+                          ("austria", 0.01), ("czech", 0.01), ("bulgaria", 0.01), ("slovenia", 0.01),
+                          ("hungary", 0.01), ("luxembourg", 0.01), ("denmark", 0.01), ("france", 0.01),
+                          ("switzerland", 0.01)],
+        "random": 0.10,
     },
     "austria": {
-        "national": 0.69,
-        "international": [("germany", 0.08), ("netherlands", 0.06), ("switzerland", 0.02), ("czech", 0.03),
-                          ("slovenia", 0.02), ("slovakia", 0.03), ("hungary", 0.03), ("italy", 0.03)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("germany", 0.08), ("poland", 0.04), ("hungary", 0.03), ("slovenia", 0.03),
+                          ("slovakia", 0.03), ("romania", 0.02), ("czech", 0.02), ("croatia", 0.02),
+                          ("lithuania", 0.01), ("switzerland", 0.01), ("italy", 0.01)],
+        "random": 0.10,
     },
     "belgium": {
-        "national": 0.79,
-        "international": [("luxembourg", 0.05), ("netherlands", 0.05), ("germany", 0.03), ("france", 0.05),
-                          ("uk", 0.02)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("netherlands", 0.07), ("luxembourg", 0.06), ("poland", 0.05), ("france", 0.04),
+                          ("romania", 0.02), ("uk", 0.02), ("lithuania", 0.01), ("germany", 0.01), ("italy", 0.01),
+                          ("hungary", 0.01)],
+        "random": 0.10,
     },
     "france": {
         "national": 0.79,
-        "international": [("netherlands", 0.05), ("spain", 0.04), ("belgium", 0.02), ("switzerland", 0.01),
-                          ("italy", 0.01), ("germany", 0.04), ("uk", 0.02), ("luxembourg", 0.01)],
-        "random": 0.01,
+        "international": [("poland", 0.06), ("belgium", 0.04), ("spain", 0.04), ("lithuania", 0.03),
+                          ("luxembourg", 0.03), ("uk", 0.02), ("romania", 0.02), ("germany", 0.02), ("italy", 0.01),
+                          ("portugal", 0.01), ("netherlands", 0.01), ("switzerland", 0.01)],
+        "random": 0.10,
     },
     "luxembourg": {
-        "national": 0.69,
-        "international": [("belgium", 0.08), ("germany", 0.07), ("netherlands", 0.06), ("france", 0.08)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("belgium", 0.06), ("france", 0.05), ("poland", 0.05), ("germany", 0.03),
+                          ("netherlands", 0.05), ("lithuania", 0.03), ("romania", 0.02), ("switzerland", 0.01)],
+        "random": 0.10,
     },
     "norway": {
-        "national": 0.79,
-        "international": [("germany", 0.05), ("netherlands", 0.04), ("denmark", 0.04), ("sweden", 0.07)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("sweden", 0.08), ("lithuania", 0.07), ("poland", 0.04), ("latvia", 0.03), ("germany", 0.03),
+                          ("denmark", 0.02), ("estonia", 0.02), ("netherlands", 0.01)],
+        "random": 0.10,
     },
     "uk": {
-        "national": 0.79,
-        "international": [("germany", 0.05), ("netherlands", 0.05), ("france", 0.04), ("belgium", 0.03),
-                          ("lithuania", 0.01), ("poland", 0.02)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("poland", 0.08), ("germany", 0.05), ("netherlands", 0.04), ("france", 0.04),
+                          ("belgium", 0.03),
+                          ("lithuania", 0.02), ("romania", 0.02), ("hungary", 0.02)],
+        "random": 0.10,
     },
     "turkey": {
-        "national": 0.79,
-        "international": [("germany", 0.05), ("netherlands", 0.005), ("bulgaria", 0.04), ("romania", 0.04),
-                          ("sweden", 0.005), ("russia", 0.04), ("macedonia", 0.005), ("bosnia", 0.005)],
-        "random": 0.01,
+        "national": 0.80,
+        "international": [("bulgaria", 0.06), ("romania", 0.03), ("poland", 0.03), ("russia", 0.02), ("serbia", 0.01)],
+        "random": 0.05,
     },
     "lithuania": {
-        "national": 0.79,
-        "international": [("latvia", 0.07), ("poland", 0.05), ("estonia", 0.04), ("uk", 0.03), ("russia", 0.01)],
-        "random": 0.01,
+        "national": 0.65,
+        "international": [("latvia", 0.08), ("poland", 0.06), ("estonia", 0.06), ("finland", 0.02), ("sweden", 0.02),
+                          ("russia", 0.01)],
+        "random": 0.10,
     },
     "bulgaria": {
-        "national": 0.79,
-        "international": [("romania", 0.07), ("hungary", 0.03), ("germany", 0.03), ("turkey", 0.02),
-                          ("macedonia", 0.02), ("serbia", 0.03)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("turkey", 0.10), ("romania", 0.08), ("hungary", 0.04), ("serbia", 0.03), ("poland", 0.03),
+                          ("macedonia", 0.02)],
+        "random": 0.10,
     },
     "poland": {
-        "national": 0.79,
-        "international": [("czech", 0.04), ("slovakia", 0.04), ("germany", 0.03), ("lithuania", 0.04), ("latvia", 0.03),
-                          ("estonia", 0.02)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("germany", 0.08), ("lithuania", 0.08), ("romania", 0.05), ("slovakia", 0.02),
+                          ("latvia", 0.02), ("czech", 0.01), ("austria", 0.01), ("slovenia", 0.01), ("hungary", 0.01),
+                          ("croatia", 0.01)],
+        "random": 0.10,
     },
     "portugal": {
-        "national": 0.79,
-        "international": [("spain", 0.06), ("france", 0.05), ("uk", 0.02), ("luxembourg", 0.01), ("netherlands", 0.03),
-                          ("germany", 0.03)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("spain", 0.07), ("romania", 0.04), ("poland", 0.04), ("france", 0.03), ("bulgaria", 0.02),
+                          ("lithuania", 0.02), ("uk", 0.02), ("luxembourg", 0.02), ("germany", 0.02), ("hungary", 0.01),
+                          ("slovenia", 0.01)],
+        "random": 0.10,
     },
     "spain": {
-        "national": 0.79,
-        "international": [("portugal", 0.06), ("france", 0.05), ("uk", 0.02), ("luxembourg", 0.01),
-                          ("netherlands", 0.03), ("germany", 0.03)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("portugal", 0.07), ("romania", 0.04), ("poland", 0.04), ("france", 0.03), ("bulgaria", 0.02),
+                          ("lithuania", 0.02), ("uk", 0.02), ("luxembourg", 0.02), ("germany", 0.02), ("hungary", 0.01),
+                          ("slovenia", 0.01)],
+        "random": 0.10,
     },
     "romania": {
-        "national": 0.79,
-        "international": [("germany", 0.05), ("italy", 0.01), ("france", 0.01), ("turkey", 0.02), ("hungary", 0.04),
-                          ("serbia", 0.03), ("poland", 0.02), ("bulgaria", 0.02)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("turkey", 0.08), ("hungary", 0.08), ("poland", 0.07), ("serbia", 0.03), ("bulgaria", 0.04)],
+        "random": 0.10,
     },
     "russia": {
-        "national": 0.89,
-        "international": [("finland", 0.035), ("estonia", 0.03), ("latvia", 0.025), ("lithuania", 0.01)],
+        "national": 0.85,
+        "international": [("lithuania", 0.06), ("latvia", 0.03), ("poland", 0.02), ("finland", 0.02),
+                          ("estonia", 0.01)],
         "random": 0.01,
     },
     "estonia": {
-        "national": 0.79,
-        "international": [("latvia", 0.05), ("lithuania", 0.03), ("finland", 0.05), ("germany", 0.03), ("poland", 0.01),
-                          ("russia", 0.03)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("latvia", 0.07), ("lithuania", 0.06), ("poland", 0.06), ("finland", 0.05), ("russia", 0.02),
+                          ("romania", 0.02), ("germany", 0.02)],
+        "random": 0.10,
     },
     "finland": {
-        "national": 0.79,
-        "international": [("estonia", 0.05), ("latvia", 0.05), ("lithuania", 0.02), ("sweden", 0.02), ("norway", 0.03),
-                          ("poland", 0.02), ("russia", 0.01)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("poland", 0.09), ("estonia", 0.06), ("germany", 0.03), ("sweden", 0.03), ("norway", 0.03),
+                          ("latvia", 0.02), ("lithuania", 0.02), ("russia", 0.02)],
+        "random": 0.10,
     },
     "hungary": {
-        "national": 0.79,
-        "international": [("romania", 0.05), ("slovakia", 0.04), ("austria", 0.02), ("serbia", 0.02),
-                          ("slovenia", 0.02), ("croatia", 0.02), ("italy", 0.01), ("germany", 0.01), ("turkey", 0.01)],
-        "random": 0.01,
-
+        "national": 0.60,
+        "international": [("poland", 0.06), ("romania", 0.06), ("slovakia", 0.05), ("romania", 0.05), ("turkey", 0.02),
+                          ("czechia", 0.01), ("germany", 0.01), ("austria", 0.01), ("croatia", 0.01), ("serbia", 0.01)],
+        "random": 0.10,
     },
     "switzerland": {
-        "national": 0.74,
-        "international": [("germany", 0.05), ("france", 0.05), ("austria", 0.05), ("italy", 0.05), ("uk", 0.03),
-                          ("netherlands", 0.02)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("germany", 0.11), ("poland", 0.07), ("italy", 0.06), ("austria", 0.01), ("slovakia", 0.01),
+                          ("france", 0.01), ("netherlands", 0.01), ("hungary", 0.01), ("lithuania", 0.01)],
+        "random": 0.10,
     },
     "latvia": {
-        "national": 0.79,
-        "international": [("lithuania", 0.06), ("estonia", 0.05), ("finland", 0.02), ("germany", 0.02),
-                          ("poland", 0.02), ("russia", 0.03)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("lithuania", 0.08), ("poland", 0.06), ("estonia", 0.06), ("finland", 0.02), ("sweden", 0.02),
+                          ("russia", 0.01)],
+        "random": 0.10,
     },
     "sweden": {
-        "national": 0.79,
-        "international": [("norway", 0.05), ("finland", 0.04), ("denmark", 0.04), ("germany", 0.03),
-                          ("netherlands", 0.03)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("poland", 0.08), ("norway", 0.04), ("denmark", 0.04), ("lithuania", 0.03), ("latvia", 0.02),
+                          ("germany", 0.02), ("estonia", 0.02), ("netherlands", 0.02), ("finland", 0.02),
+                          ("romania", 0.01)],
+        "random": 0.10,
     },
     "denmark": {
-        "national": 0.79,
-        "international": [("germany", 0.06), ("sweden", 0.05), ("norway", 0.05), ("netherlands", 0.04)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("poland", 0.08), ("norway", 0.05), ("sweden", 0.05), ("germany", 0.04), ("lithuania", 0.03),
+                          ("romania", 0.02), ("netherlands", 0.02), ("czechia", 0.01)],
+        "random": 0.10,
     },
     "czech": {
-        "national": 0.79,
-        "international": [("germany", 0.04), ("poland", 0.05), ("austria", 0.04), ("slovakia", 0.04),
-                          ("hungary", 0.03)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("poland", 0.09), ("slovakia", 0.09), ("germany", 0.04), ("lithuania", 0.02),
+                          ("hungary", 0.02), ("romania", 0.01), ("austria", 0.02), ("netherlands", 0.01)],
+        "random": 0.10,
     },
     "italy": {
-        "national": 0.79,
-        "international": [("germany", 0.06), ("netherlands", 0.04), ("switzerland", 0.02), ("france", 0.03),
-                          ("slovenia", 0.02), ("austria", 0.03)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("poland", 0.06), ("romania", 0.04), ("france", 0.04), ("lithuania", 0.03), ("germany", 0.03),
+                          ("slovenia", 0.03), ("austria", 0.03), ("bulgaria", 0.03), ("slovakia", 0.02),
+                          ("switzerland", 0.01)],
+        "random": 0.10,
     },
     "slovakia": {
-        "national": 0.79,
-        "international": [("czech", 0.04), ("poland", 0.05), ("austria", 0.04), ("germany", 0.03), ("hungary", 0.04)],
-        "random": 0.01,
+        "national": 0.60,
+        "international": [("czechia", 0.12), ("poland", 0.09), ("hungary", 0.07), ("austria", 0.01), ("germany", 0.1)],
+        "random": 0.10,
     },
-    "albania": {
+    "albania": {  ## TODO: configure below
         "national": 0.79,
         "international": [("montenegro", 0.05), ("kosovo", 0.05), ("macedonia", 0.06), ("germany", 0.01),
                           ("serbia", 0.03)],
@@ -249,7 +264,6 @@ spawn_config = {
     },
 }
 
-
 truck_type_string = "truck"
 trailer_type_string = "trailer"
 
@@ -260,11 +274,14 @@ country_lps = create_lp_defs(country_abbreviations, ["car", "truck", "trailer"])
 # get all specified trucks from traffic
 truck_variant_dict, trucks_per_country = get_vehicles_per_country(country_abbreviations, vanilla_truck_list)
 # get all specified vanilla trailers from traffic
-trailer_variant_dict, trailer_per_country = get_vehicles_per_country(country_abbreviations, vanilla_trailer_list, src_dir=trailer_src_dir)
+trailer_variant_dict, trailer_per_country = get_vehicles_per_country(country_abbreviations, vanilla_trailer_list,
+                                                                     src_dir=trailer_src_dir)
 # make a trailer def for each trailer for each country, set custom license plate
-create_vehicle_traffic_defs(trailer_per_country, trailer_variant_dict, trailer_type_string, src_dir=trailer_src_dir, dst_dir=trailer_dst_dir)
-# make a truck def for each truck for each country, set spawn_ratio to 0, custom license plate and link with all country trailers via trailer_chains
-create_vehicle_traffic_defs(trucks_per_country, truck_variant_dict, truck_type_string, trailer_chains=trailer_per_country)
+create_vehicle_traffic_defs(trailer_per_country, trailer_variant_dict, trailer_type_string, src_dir=trailer_src_dir,
+                            dst_dir=trailer_dst_dir)
+# make a truck def for each truck for each country and link with all country trailers via trailer_chains
+create_vehicle_traffic_defs(trucks_per_country, truck_variant_dict, truck_type_string,
+                            trailer_chains=trailer_per_country)
 # create all other country related files for the trucks
 create_country_data(country_abbreviations, trucks_per_country, truck_type_string, country_lps, spawn_config)
 # create all other country related files for the trailers
